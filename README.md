d20c2bfdb330208a2683426f2f49a42fcd0dc1ff
mikekoma <mike@s-m-l.org>
2023-04-17 23:23:57 +0900

    initial import

--- README.md
+++ README.md
@@ -1,3 +1,46 @@
+# Title
+
+Seeduino XIAO HID1Switch Locker
+
+
+# Overview
+
+Lock your desktop with one switch.
+
+
+# Description
+
+## Hardware
+
+Seeduino XIAO SAMD21
+[Seeed Studio XIAO SAMD21(Seeeduino XIAO) - Arduino Microcontroller - SAMD21 Cortex M0+ with Free Course - 5V/16MHz](https://www.seeedstudio.com/Seeeduino-XIAO-Arduino-Microcontroller-SAMD21-Cortex-M0+-p-4426.html)
+
+> 配線図
+>
+>  +-------SW--------+
+>  |  _____________  |
+>  +--D0   USB   5V  |
+>     D1        GND--+
+>     D2        3V3
+>     D3        D10
+>     D4         D9
+>     D5         D8
+>     D6_________D7
+
+
+## Software
+
+### スイッチ押した時
+Shiftキー送信(スクリーンセーバー解除)
+
+### スイッチ離した時
+Win+Lキー送信(画面ロック)
+
+### 制限
+キー送信した後、1秒間はスイッチの変化検出を行わない。
+
+# Licence
+
 MIT License
 
 Copyright (c) 2023 Suns & Moon Laboratory
@@ -19,3 +62,11 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+
+
+# Author
+
+Suns & Moon Laboratory  
+https://www.s-m-l.org  
+https://github.com/mikekoma/  
+https://twitter.com/mikekoma  
---
+++ code.py
@@ -0,0 +1,85 @@
+#
+# 1 Key Keyboard
+#
+# Circuit Diagram
+#  XIAO SAMD21
+#
+#  +-------SW--------+
+#  |  _____________  |
+#  +--D0   USB   5V  |
+#     D1        GND--+
+#     D2        3V3
+#     D3        D10
+#     D4         D9
+#     D5         D8
+#     D6_________D7
+#
+# Circuit Python 8.0.5
+# https://circuitpython.org/board/seeeduino_xiao/
+#
+# Adafruit_CircuitPython_Bundle 20230416
+# After extracting the zip, copy lib/adafruit_hid to CIRCUITPY/lib
+# https://github.com/adafruit/Adafruit_CircuitPython_Bundle/releases
+#
+# MIT Licence
+# Copyright (c) 2023 Suns & Moon Laboratory
+# https://opensource.org/licenses/mit-license.php
+
+# Import the libraries
+import time
+import board
+from digitalio import DigitalInOut, Direction, Pull
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keycode import Keycode
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+import usb_hid
+
+STT_L_KEY_HI = 1
+STT_L_KEY_DOWN = 2
+STT_L_KEY_LO = 3
+STT_L_KEY_UP = 4
+
+class LoKey:
+		def __init__(self):
+			self.state = STT_L_KEY_HI
+		
+		def update(self, keybit):
+			if self.state == STT_L_KEY_HI:
+				if keybit == 0:
+					time.sleep(0.01)
+					if keybit == 0:
+						self.state = STT_L_KEY_DOWN
+			elif self.state == STT_L_KEY_DOWN:
+				kbd.send(Keycode.GUI ,Keycode.L)
+				led.value = False # led on
+				self.state = STT_L_KEY_LO
+			elif self.state == STT_L_KEY_LO:
+				if keybit == 1:
+					self.state = STT_L_KEY_UP
+			elif self.state == STT_L_KEY_UP:
+				kbd.send(Keycode.SHIFT)
+				led.value = True # led off
+				self.state = STT_L_KEY_HI
+			else:
+				elifself.state = STT_L_KEY_HI
+
+lokey = LoKey()
+
+# define output LED
+led = DigitalInOut(board.LED_INVERTED)
+led.direction = Direction.OUTPUT
+
+# led off
+led.value = True
+
+# configure device as keyboard
+kbd = Keyboard(usb_hid.devices)
+layout = KeyboardLayoutUS(kbd)
+
+io_keysw = DigitalInOut(board.D0)
+io_keysw.direction = Direction.INPUT
+io_keysw.pull = Pull.UP
+
+# loop forever
+while True:
+	lokey.update(io_keysw.value)
